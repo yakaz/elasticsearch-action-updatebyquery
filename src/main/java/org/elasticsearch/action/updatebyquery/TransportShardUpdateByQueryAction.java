db7a45fd1c81142340e647b1e2e0609959a18fce
Martijn van Groningen <martijn.v.groningen@gmail.com>
2014-03-25 13:01:01 +0700

    Use SC#clearAndRelease() instead of SC#release()

--- src/main/java/org/elasticsearch/action/updatebyquery/TransportShardUpdateByQueryAction.java
+++ src/main/java/org/elasticsearch/action/updatebyquery/TransportShardUpdateByQueryAction.java
@@ -171,7 +171,7 @@ public class TransportShardUpdateByQueryAction extends TransportAction<ShardUpda
             if (docsToUpdateCount == 0) {
                 ShardUpdateByQueryResponse response = new ShardUpdateByQueryResponse(request.shardId());
                 listener.onResponse(response);
-                searchContext.release();
+                searchContext.clearAndRelease();
                 return;
             }
             BatchedShardUpdateByQueryExecutor bulkExecutor = new BatchedShardUpdateByQueryExecutor(
@@ -181,7 +181,7 @@ public class TransportShardUpdateByQueryAction extends TransportAction<ShardUpda
         } catch (Throwable t) {
             // If we end up here then BatchedShardUpdateByQueryExecutor#finalizeBulkActions isn't invoked
             // so we need to release the search context.
-            searchContext.release();
+            searchContext.clearAndRelease();
             listener.onFailure(t);
         } finally {
             SearchContext.removeCurrent();
@@ -328,7 +328,7 @@ public class TransportShardUpdateByQueryAction extends TransportAction<ShardUpda
         }
 
         private void finalizeBulkActions(Throwable e) {
-            updateByQueryContext.searchContext.release();
+            updateByQueryContext.searchContext.clearAndRelease();
             BulkItemResponse[] bulkResponses = receivedBulkItemResponses.toArray(new BulkItemResponse[receivedBulkItemResponses.size()]);
             receivedBulkItemResponses.clear();
             ShardUpdateByQueryResponse finalResponse = new ShardUpdateByQueryResponse(
